e0adcaa50b1e55020ce1c947dd96264aa873b630
RawFisher <supermen12345@126.com>
2017-12-18 13:32:29 +0800

    新闻页面

--- app/src/main/java/com/example/a201711116/fragment/NewsActivity.java
+++ app/src/main/java/com/example/a201711116/fragment/NewsActivity.java
@@ -1,10 +1,11 @@
 package com.example.a201711116.fragment;
 
-
-
-
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,66 +24,58 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
-import java.io.IOException;
-
-
 public class NewsActivity extends Fragment {
-
     String target = "http://dean.swjtu.edu.cn/rss.jsp";
-    String result;
     Document doc;
     private TextView mText;
 
-
-
-    //初始化View
-    private void findView(View view) {
-
-        mText = view.findViewById(R.id.password);
-
-    }
-
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-
         View view = inflater.inflate(R.layout.activity_news,null);
-        findView(view);
-        getNews();
-        return  view;
+        mText = view.findViewById(R.id.news_item);
+        new Thread(getNews).start();
+        return view;
     }
 
-    //显示新闻
-    public void getNews(){
-        String ss = null;
-        try
-        {
-            doc = Jsoup.connect(target).get();
-            System.out.println(doc.html());
+    private Handler handle = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    Document doc = (Document)msg.obj;
+                    Elements link = doc.select("link");
+                    Elements title = doc.select("title");
+                    for(int i=1;i<link.size();i++)
+                    {
+                        mText.append(Html.fromHtml("<a href='" + link.get(i).html() + "'>"
+                        + title.get(i).html() + "</a>"));
+                        mText.append("\n\n");
+                    }
+                    break;
+            }
+        };
+    };
 
-//            Elements link = doc.select("link");//取得所有的url
-//            Elements title = doc.select("title");
-//            for(int i=1;i<link.size();i++)
-//            {
-//                ss = title.get(i).html().toString();
-//                mText.append(ss);
-//                System.out.println(ss);
-//            }
-//            System.out.println(link.get(0).html());
-//            System.out.println(title.get(0));
+    Runnable getNews = new Runnable() {
+        @Override
+        public void run() {
+            try
+            {
+                doc = Jsoup.connect(target).get();
+                Message msg = new Message();
+                msg.what = 0;
+                msg.obj = doc;
+                handle.sendMessage(msg);
+            }
+            catch(MalformedURLException e)
+            {
+                System.out.println(e);
+            }
+            catch(IOException e)
+            {
+                System.out.println(e);
+            }
         }
-        catch(MalformedURLException e)
-        {
-            System.out.println(e);
-        }
-        catch(IOException e)
-        {
-            System.out.println(e);
-        }
-    }
+    };
 
 
 }
